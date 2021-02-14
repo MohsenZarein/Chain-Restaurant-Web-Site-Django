759e9b48f1c106fbb991cd18d10161be473f01b9
mohsen <mohsenzarein1379@gmail.com>
2021-02-14 22:48:01 +0330

    configured database

--- chain_restaurant/settings.py
+++ chain_restaurant/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'chain_restaurant.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'chain_restaurant_db',
+        'USER': 'postgres',
+        'PASSWORD': '0260psql',
+        'HOST': 'localhost'
     }
 }
 
