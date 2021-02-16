c890830da14d54b57a3a32a63d1caa2053a2c8af
mohsen <mohsenzarein1379@gmail.com>
2021-02-16 15:31:22 +0330

    passed the tests for creating Customer & Personnel models

--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.core import exceptions
 
 from core import models
 
@@ -50,3 +51,112 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    
+    def test_create_customer(self):
+        """ Test creating a new customer """
+        user = get_user_model().objects.create_user(
+            email='test@gmail.com',
+            password='123456'
+        )
+        customer = models.Customer.objects.create(
+                user=user,
+                customer_id=73478643,
+                gender='male',
+                province='Alborz',
+                city='karaj',
+                street='street',
+                alley='alley'
+            )
+        
+        self.assertEqual(user.email,customer.user.email)
+
+
+
+    def test_create_customer_already_exists(self):
+        """ Test creating a customer that already exists """
+        user = get_user_model().objects.create_user(
+            email='test@gmail.com',
+            password='123456'
+        )
+        customer1 = models.Customer.objects.create(
+                user=user,
+                customer_id=73478643,
+                gender='male',
+                province='Alborz',
+                city='karaj',
+                street='street',
+                alley='alley'
+            )
+        with self.assertRaises(Exception):
+            customer2 = models.Customer.objects.create(
+                    user=user,
+                    customer_id=73478643,
+                    gender='male',
+                    province='Alborz',
+                    city='karaj',
+                    street='street',
+                    alley='alley'
+                )
+
+
+
+    def test_create_personnel(self):
+        """ Test creating a new personnel """
+        user = get_user_model().objects.create_user(
+            email='test@gmail.com',
+            password='123456'
+        )
+        user.is_staff = True
+
+        personnel = models.Personnel.objects.create(
+                user=user,
+                personnel_code=445455,
+                gender='male',
+                province='Alborz',
+                city='karaj',
+                street='street',
+                alley='alley',
+                birth_date='2015-11-23',
+                age=6,
+                salary=10000.25
+            )
+        
+        self.assertEqual(user.email, personnel.user.email)
+
+    
+
+    def test_create_personnel_already_exists(self):
+        """ Test creating a new personnel """
+        user = get_user_model().objects.create_user(
+            email='test@gmail.com',
+            password='123456'
+        )
+        user.is_staff = True
+
+        personnel1 = models.Personnel.objects.create(
+                user=user,
+                personnel_code=445455,
+                gender='male',
+                province='Alborz',
+                city='karaj',
+                street='street',
+                alley='alley',
+                birth_date='2015-11-23',
+                age=6,
+                salary=1000.25
+            )
+        with self.assertRaises(Exception):
+            personnel2 = models.Personnel.objects.create(
+                    user=user,
+                    personnel_code=445455,
+                    gender='male',
+                    province='Alborz',
+                    city='karaj',
+                    street='street',
+                    alley='alley',
+                    birth_date='2015-11-23',
+                    age=6,
+                    salary=1000.25
+                )
+        
